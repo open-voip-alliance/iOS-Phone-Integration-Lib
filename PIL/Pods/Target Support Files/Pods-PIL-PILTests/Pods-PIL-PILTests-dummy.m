126110c8cc1c3e2ad1fcbd9361cf1cd09254f2d2
Chris Kontos <chris.kontos@wearespindle.com>
2020-12-01 18:56:33 +0100

    podspec and license

---
+++ PIL/Pods/Target Support Files/Pods-PIL-PILTests/Pods-PIL-PILTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PIL_PILTests : NSObject
+@end
+@implementation PodsDummy_Pods_PIL_PILTests
+@end
---
+++ PIL/Pods/Target Support Files/Pods-PIL/Pods-PIL-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PIL : NSObject
+@end
+@implementation PodsDummy_Pods_PIL
+@end
