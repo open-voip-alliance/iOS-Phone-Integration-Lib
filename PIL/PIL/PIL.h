b863f79c719a19a820d68c0513855c39715ca726
Chris Kontos <chris.kontos@wearespindle.com>
2020-12-01 18:49:11 +0100

    Initial commit

---
+++ PIL/PIL/PIL.h
@@ -0,0 +1,18 @@
+//
+//  PIL.h
+//  PIL
+//
+//  Created by Chris Kontos on 01/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PIL.
+FOUNDATION_EXPORT double PILVersionNumber;
+
+//! Project version string for PIL.
+FOUNDATION_EXPORT const unsigned char PILVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PIL/PublicHeader.h>
+
+
