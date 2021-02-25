37f6fd0b45235c1259f3576e8d29456a31934185
Chris Kontos <chris.kontos@wearespindle.com>
2021-02-25 22:05:07 +0100

    snapshoting PILCall

--- PIL/PIL/Calls/PILCall.swift
+++ PIL/PIL/Calls/PILCall.swift
@@ -9,76 +9,48 @@ public struct PILCall {
     let direction: CallDirection
     let uuid: UUID
     let session: Session
-
-    public var state: CallState {
-        get {
-            switch session.state {
-            case .idle:
-                return .initializing
-            case .incomingReceived, .outgoingDidInitialize, .outgoingProgress, .outgoingRinging:
-                return .ringing
-            case .connected, .streamsRunning, .outgoingEarlyMedia, .earlyUpdatedByRemote, .earlyUpdating, .incomingEarlyMedia, .pausing, .resuming, .referred, .updatedByRemote, .updating:
-                return .connected
-            case .paused:
-                return .heldByLocal
-            case .pausedByRemote:
-                return .heldByRemote
-            case .error:
-                return .error
-            case .ended, .released:
-                return .ended
-            }
-        }
-    }
+    var state: CallState!
+    let isIncoming: Bool
+    public let isOnHold: Bool
+    let duration: Int
+    public let sessionState: SessionState
+    let remoteNumber: String
+    let displayName: String
+    let mos: Float
+    //TODO: let contact: Bool?
     
-    var isIncoming: Bool {
-        get {
-            self.direction == CallDirection.inbound
-        }
-    }
-    
-    public var isOnHold: Bool {
-        get {
-            self.state == .heldByRemote || self.state == .heldByLocal
-        }
-    }
-
-    var duration: Int {
-        get { session.durationInSec ?? 0 }
-    }
-
-    public var sessionState: SessionState {
-        get { session.state }
-    }
-
-    var remoteNumber: String {
-        get { session.remoteNumber }
-    }
-
-    var displayName: String? {
-        get { session.displayName }
-    }
-    
-    var mos: Float {
-        get {session.getAverageRating()}
-    }
-    
-    var contanct: Bool? {
-        //TODO: Implement contact check
-        get {false}
-    }
-
-    init(session: Session, direction: CallDirection) {
+    init(session: Session, direction: CallDirection, uuid:UUID = UUID.init()) {
         self.direction = direction
-        self.uuid = UUID.init()
         self.session = session
-    }
-
-    init(session: Session, direction: CallDirection, uuid: UUID) {
-        self.direction = direction
         self.uuid = uuid
-        self.session = session
+        
+        state = PILCall.convertCallState(session: session)
+        isIncoming = direction == CallDirection.inbound
+        isOnHold = (session.state == .pausedByRemote || session.state == .paused)
+        duration = session.durationInSec ?? 0
+        sessionState = session.state
+        remoteNumber = session.remoteNumber
+        displayName = session.displayName ?? ""
+        mos = session.getAverageRating()
+        //TODO: contact =
+    }
+
+    private static func convertCallState(session: Session) -> CallState {
+        switch session.state {
+        case .idle:
+            return .initializing
+        case .incomingReceived, .outgoingDidInitialize, .outgoingProgress, .outgoingRinging:
+            return .ringing
+        case .connected, .streamsRunning, .outgoingEarlyMedia, .earlyUpdatedByRemote, .earlyUpdating, .incomingEarlyMedia, .pausing, .resuming, .referred, .updatedByRemote, .updating:
+            return .connected
+        case .paused:
+            return .heldByLocal
+        case .pausedByRemote:
+            return .heldByRemote
+        case .error:
+            return .error
+        case .ended, .released:
+            return .ended
+        }
     }
 }
-
-
